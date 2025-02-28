4a85bd144704161eca46cdcc10c5f769212bd791
Brunin16 <bruedu16@gmail.com>
2025-02-28 00:25:44 -0300

    fix: adding  to calculateTime the case of amost years contribution

--- Tab_retirement_calculator/Service/RetirementCalculator.cs
+++ Tab_retirement_calculator/Service/RetirementCalculator.cs
@@ -11,33 +11,45 @@ namespace Tab_retirement_calculator.Service
         public string responseYear(int year, int workedYears, string gender, double averageSalary)
         {
             int minYear = gender == "H" ? 65 : 62;
-            int remainingTime = calculateTime(year, workedYears, gender, minYear);
+            int minWorkedYearsBefore = gender == "H" ? 35 : 30;
+            int remainingTime = calculateTime(year, workedYears, gender, minYear, minWorkedYearsBefore);
             if (remainingTime == -1)
             {
                 return "Verifique as informações oferecidas\nO tempo trabalhado ou idade esta abaixo de 0";
             }
             if (remainingTime == -2)
             {
-                return $"A idade minima para a aposentadoria é de {minYear} anos";
+                return $"A idade minima para a aposentadoria é de {minYear} anos\nOu no minimo {minWorkedYearsBefore} anos de contribuição ";
             }
             double value = calculateValue(averageSalary, workedYears, minYear);
             if (remainingTime == 0)
             {
                 return $"Parabéns, você já pode se aposentar\nE receberá R${value}";
             }
+            if (remainingTime == -3)
+            {
+                return "Parabéns, você já pode se aposentar\nE receberá R$"+ value;
+            }
             return "Faltam " + remainingTime + " anos para a sua aposentadoria\nE voce reberá em media R$" + value;
         }
-        public int calculateTime(int year, int workedYears, string gender, int minYear) 
+        public int calculateTime(int year, int workedYears, string gender, int minYear, int minWorkedYearsBefore) 
         {     
             int minWorkedYears = gender == "H" ? 20 : 15;
+
             if (year < 0 || workedYears < 0)
             {
                 return -1;
+            }
+            if (workedYears >= minWorkedYearsBefore)
+            {
+                return -3;
             }
             if (year < minYear)
             {
                 return -2;
             }
+
+
             return (minWorkedYears - workedYears) < 0 ? 0 : minWorkedYears - workedYears; 
         }
         public double calculateValue(double averageSalary, int workedYears, int minYear)
